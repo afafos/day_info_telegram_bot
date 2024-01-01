e9ab53060f9f8921a94e18c8840eff4f97af6bd4
afafos <afanasyev239@gmail.com>
2024-01-01 12:38:29 +0300

    Get coordinates by name of location

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
-telegram_api = '6756741753:AAEEvn9EGOOvVa47GZDXN8OqUzuoA9cjcMY'
-openweather_api = '70ace1e09cefbe1713f28e1b8cae72da'
+tg_api = '6756741753:AAEEvn9EGOOvVa47GZDXN8OqUzuoA9cjcMY'
+opweather_api = '70ace1e09cefbe1713f28e1b8cae72da'
+mapq_api = 'C4AbGDp5IugUBKmLeFricsHtJxKLeJeq'
 # coordinates    59.92555503268884 30.294758164598026
--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import telebot
 import json
 import requests
-from datetime import datetime
-from telebot import types
 from datetime import datetime, timedelta
 from api import *
 
-
-OpWeather_api = openweather_api
+# Replace with your actual API keys
+openweather_api = opweather_api
+mapquest_api = mapq_api
+telegram_api = tg_api
 
 bot = telebot.TeleBot(telegram_api)
 
 
+def get_coordinates_by_address(address):
+    base_url = "http://www.mapquestapi.com/geocoding/v1/address"
+    params = {"key": mapquest_api, "location": address}
+    response = requests.get(base_url, params=params)
+    data1 = response.json()
+
+    if data1["results"]:
+        location = data1["results"][0]["locations"][0]
+        lat = location["latLng"]["lat"]
+        lng = location["latLng"]["lng"]
+        return lat, lng
+    else:
+        return None
+
+
 def get_current_weather_info(latitude, longitude):
-    url = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={OpWeather_api}'
+    url = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={openweather_api}'
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -24,7 +39,7 @@ def get_current_weather_info(latitude, longitude):
 
 
 def get_forecast_info(latitude, longitude):
-    url = f'https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid={OpWeather_api}'
+    url = f'https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid={openweather_api}'
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -100,8 +115,8 @@ def handle_start(message):
 def handle_find(message):
     today_date = datetime.now().strftime("%d-%m-%Y")
 
-    markup = types.InlineKeyboardMarkup()
-    button = types.InlineKeyboardButton(text="Get Weather Forecast", callback_data="weather_forecast")
+    markup = telebot.types.InlineKeyboardMarkup()
+    button = telebot.types.InlineKeyboardButton(text="Get Weather Forecast", callback_data="weather_forecast")
     markup.add(button)
 
     bot.send_message(message.chat.id, f"Today's date: {today_date}", reply_markup=markup)
@@ -110,15 +125,17 @@ def handle_find(message):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_handler(call):
     if call.data == "weather_forecast":
-        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
         bot.send_message(call.message.chat.id, "Please share your location:", reply_markup=keyboard)
 
 
 @bot.message_handler(func=lambda message: True, content_types=["text"])
 def handle_text(message):
     try:
-        coordinates = [float(coord) for coord in message.text.split(',')]
-        if len(coordinates) == 2:
+        address = message.text
+        coordinates = get_coordinates_by_address(address)
+
+        if coordinates:
             user_id = message.from_user.id
             coordinates_dict = {"user_coordinates": {"latitude": coordinates[0], "longitude": coordinates[1]}}
             with open(f"user_{user_id}_coordinates.json", "w") as json_file:
@@ -130,8 +147,7 @@ def handle_text(message):
             forecast_info = get_forecast_info(coordinates[0], coordinates[1])
             bot.send_message(message.chat.id, "Weather Forecast:\n\n" + forecast_info)
         else:
-            bot.send_message(message.chat.id,
-                             "Invalid coordinates format. Please enter latitude and longitude separated by a comma.")
+            bot.send_message(message.chat.id, f"Failed to get coordinates for the location: {address}")
     except ValueError:
         bot.send_message(message.chat.id, "Invalid coordinates format. Please enter valid numeric values.")
 
